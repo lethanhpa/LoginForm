5ed2af30804a62d4f8839aacb08f3527915705df
lethanhpa <lethanhpa1406@gmail.com>
2024-07-26 17:56:40 +0700

    login form

---
+++ login-form/src/Pages/Account/LoginPage.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Button, Form, Input } from "antd";
+import './LoginPage.css';
+
+const LoginPage = () => {
+  return (
+    <div className="login_background">
+      <div className="login_form_container">
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <div className="forgot_password">
+            <a href="/forgot-password">Forgot Password?</a>
+          </div>
+
+          <Form.Item className="btn_submit">
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
